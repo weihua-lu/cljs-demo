281c48fba321535e29d35b6547276c3a05b2db1d
Weihua Lu <luwh364@gmail.com>
2021-01-14 01:22:34 +0800

    Simple linkshere

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -1,16 +1,58 @@
 (ns app.client
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.core :as r]
-            [reagent.dom :as rd]))
+            [reagent.dom :as rd]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
 
-(defn some-component []
-  [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-        " text."]])
+(defn atom-input [value]
+  [:input {:type "text"
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn search [val results]
+  (go (let [_        (reset! results {:searching true})
+            response (<! (http/get "https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=linkshere"
+                                   {:with-credentials? false
+                                    :query-params      {"titles" (str val)}}))]
+        (prn (:body response))
+        (reset! results response))))
+
+(comment
+  (def a (r/atom "linux"))
+  (def result (r/atom {}))
+  (search a result)
+  @result
+  )
+
+(defn title-lists [result]
+  (when (== 200 (:status @result))
+    (let [pages  (-> @result :body :query :pages)
+          titles (->> (vals pages)
+                      (mapcat :linkshere)
+                      (map :title))]
+      [:ul
+       (for [title titles]
+         ^{:key title} [:li title])])))
+
+(defn loading-bar [result]
+  (when (:searching @result)
+    [:p "Searching, please wait....."]))
+
+(defn search-button [val response]
+  [:input {:type "button" :value "Search!"
+           :on-click #(search val response)}])
+
+(defn shared-state []
+  (let [val (r/atom "")
+        result (r/atom {})]
+    (fn []
+      [:div
+       [:p [atom-input val] [search-button @val result]]
+       [loading-bar result]
+       [title-lists result]])))
 
 (defn ^:export run
   []
   (js/console.log "Run starttt")
-  (rd/render [some-component] (js/document.getElementById "app")))
+  (rd/render [shared-state] (js/document.getElementById "app")))
